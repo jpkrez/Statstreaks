6692db684d79eafda145eccdb7d71d827df08e50
JP <jpkrez26@gmail.com>
2023-08-04 11:46:55 -0500

    Deploying to GitHub Pages

--- app.js
+++ app.js
@@ -12,41 +12,6 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-async function fetchScheduleData() {
-  try {
-    const response = await Axios.get('https://www.callofdutyleague.com/en-us/schedule');
-    print(response)
-    if (!response.data) {
-      throw new Error('Empty response received');
-    }
-    return response.data;
-  } catch (error) {
-    throw new Error('Failed to fetch schedule data');
-  }
-}
-
-app.get('/api/schedule', async (req, res) => {
-  try {
-    const scheduleData = await fetchScheduleData();
-    // Process the scheduleData as needed
-    // You might need to use a specific library like Cheerio to parse the HTML and extract the schedule information.
-    // For simplicity, let's assume the scheduleData contains the relevant schedule information as an array of objects.
-
-    const dataArray = [
-      { date: '2023-08-01', team1: 'Team A', team2: 'Team B', time: '12:00 PM' },
-      // Add more schedule information here...
-    ];
-
-    res.json(dataArray);
-  } catch (error) {
-    console.error('Error fetching schedule data:', error);
-    res.status(500).json({ error: 'Failed to fetch or process schedule data' });
-  }
-});
-
-
-
-
 async function fetchData() {
     try {
         const response = await Axios.get('https://github.com/jpkrez/Statstreaks/raw/master/public/mw2_player_stats2023.csv', { responseType: 'text' });
